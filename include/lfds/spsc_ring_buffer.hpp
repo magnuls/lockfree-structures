e22883c448e6538b4fab05a484df64b53caa74b2
magnus <magnuspicataggio@gmail.com>
2026-09-08 23:17:31 -0400

    CMake is now C++23 and have a types.hpp for custom type ailiasing

---
+++ include/lfds/spsc_ring_buffer.hpp
@@ -0,0 +1,5 @@
+#ifndef SPSC_RING_BUFFER_HPP
+#define SPSC_RING_BUFFER_HPP
+#include <types.hpp>
+
+#endif
---
+++ include/types.hpp
@@ -0,0 +1,11 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <cstdint>
+
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+using f32 = float;
+using f64 = double;
+
+#endif
