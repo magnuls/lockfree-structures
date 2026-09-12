e42a26e621ea4f00fc9ed9404493ec5af72fc935
magnus <magnuspicataggio@gmail.com>
2026-09-12 01:01:02 -0400

    Added a usize typedef for std::size_t, and unsigned 32/64 bit ints

--- include/types.hpp
+++ include/types.hpp
@@ -2,9 +2,13 @@
 #define TYPES_HPP
 
 #include <cstdint>
+#include <utility>
 
 using i32 = std::int32_t;
 using i64 = std::int64_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using usize = std::size_t;
 using f32 = float;
 using f64 = double;
 
