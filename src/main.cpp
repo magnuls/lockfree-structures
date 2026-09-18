6dc569e147fd39940723bcc20144efa08f3c8b3b
magnus <magnuspicataggio@gmail.com>
2026-09-17 21:34:17 -0400

    Templated to include a custom Allocator in which we will make. Throwing destructors are are rejected because there is no way to handle a failure in the middle of a cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -27,13 +27,20 @@ inline constexpr std::size_t CacheLine = 64;
    - slots in [consumer_pos_, producer_pos_) hold constructed objects; all others are
      raw
  */
-template<typename T, usize N>
+template<typename T, typename Allocator = std::allocator<T>>
+    requires std::is_nothrow_destructible_v<T>
 class SpscRingBuffer {
-    static_assert(is_power_of_two(N));
+    // Don't want hidden mutex's, breaks class being lock free
     static_assert(std::atomic<usize>::is_always_lock_free);
 
   public:
-    SpscRingBuffer() = default;
+    SpscRingBuffer(usize capacity, const Allocator& a = Allocator())
+        : capacity_(capacity), alloc_(a), mask_(capacity - 1) {
+        if (!is_power_of_two(capacity_))
+            throw std::invalid_argument("capacity must be a power of two");
+        // Allocate capacity
+        ptr_ = std::allocator_traits<Allocator>::allocate(alloc_, capacity_);
+    }
     ~SpscRingBuffer() {
         // Destroy everything in the range of
         //[consumer_pos_, producer_pos_)
